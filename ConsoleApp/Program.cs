f90dbbdcab6588a7be4babfc9bfd63152f309074
day12may <day12may@gmail.com>
2023-04-21 11:19:49 +0300

    So...One_more_time

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Какой класс Вы хотите создать: ");
+                switch (Console.ReadLine())
+                {
+                   
+                    default: Console.WriteLine("Такой класс не реализован"); break;
+
+                }
+            }
+        }
+
+    }
+}
